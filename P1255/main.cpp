c23f2fdbf1095fa7d6fc62c97cb8710f9dc424cf
yuzec <yuzec@163.com>
2020-12-10 10:59:33 +0800

    add P1255

---
+++ P1255/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+string add(string x, string y) {
+    int lenx = x.size();
+    int leny = y.size();
+    if (lenx > leny)
+        for (int i=0; i<lenx-leny; ++i)
+            y = '0' + y;
+    else
+        for (int i=0; i<lenx-leny; ++i)
+            x = '0' + x;
+    int jw = 0;
+    for (int i=max(lenx, leny)-1; i>=0; --i) {
+        x[i] += (y[i] - '0') +jw;
+        jw = (x[i] - '0') / 10;
+        x[i] = (x[i] - '0') % 10 + '0';
+    }
+    if (jw != 0)
+        x = char(jw + '0') + x;
+    return x;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    string a[n+1]={"1", "1"};
+    for (int i=2; i<=n; ++i)
+        a[i] = add(a[i-1],  a[i-2]);
+    a[0] = "0";
+    cout << a[n] << endl;
+    return 0;
+}
